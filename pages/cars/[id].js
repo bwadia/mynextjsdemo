13de3f33e785968ca96981ed5116bdbbc86e0472
Brahim W <wadia.brahim@gmail.com>
2022-04-10 15:22:08 +0200

    Add README

---
+++ pages/cars/[id].js
@@ -0,0 +1,62 @@
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Car({ car }) {
+
+    const router = useRouter()
+    const { id } = router.query
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>{car.color} {car.id}</title>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    {id}
+                </h1>
+
+                <Image alt="image" src={car.image} width="300px" height="300px" />
+                <Link href="/cars"><a>Cars</a></Link>
+            </main>
+        </div>
+    )
+}
+
+
+export async function getServerSideProps({ params }) {
+    const req = await fetch(`http://localhost:3000/${params.id}.json`);
+    const data = await req.json();
+
+    return {
+        props: { car: data },
+    }
+}
+
+// export async function getStaticProps({ params }) {
+
+//     const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//     const data = await req.json();
+
+//     return {
+//         props: { car: data },
+//     }
+// }
+
+// export async function getStaticPaths() {
+
+//     const req = await fetch('http://localhost:3000/cars.json');
+//     const data = await req.json();
+
+//     const paths = data.map(car => {
+//         return { params: { id: car } }
+//     })
+
+//     return {
+//         paths,
+//         fallback: false
+//     };
+// }
